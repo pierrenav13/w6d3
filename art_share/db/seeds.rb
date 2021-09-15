ee682d6b9fec8d8b13e765d98232f3d4dc67d319
pierre <pnavarin@gmail.com>
2021-09-15 17:32:26 -0400

    Started adding seeds to seed.rb

--- art_share/db/seeds.rb
+++ art_share/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user_1 = User.create(username: 'Wood')
+artwork_1 = Artwork.create(title: 'Starry Night', image_url: 'url1', artist_id: 1)
+
+
+user_2 = User.create(username: 'Davinci')
+artwork_2 = Artwork.create(title: 'Mona Lisa', image_url: 'url2', artist_id: 2)
+
+user_3 = User.create(username: 'User3')
+
+artwork_3 = Artwork.create(title: 'Vetruvian Man', image_url: 'url3', artist_id: 2)
